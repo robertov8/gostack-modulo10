64c9223d0f3d105f8c3dfb70d4e80deddf6a946b
Roberto Ribeiro <robertov82008@gmail.com>
2020-04-26 18:36:02 -0300

    06 - E-mail duplicado

--- server/__tests__/example.test.js
+++ server/__tests__/example.test.js
@@ -5,6 +5,5 @@ function soma(a, b) {
 test('if i call soma function with 4 and 5 it should return 9', () => {
   const result = soma(4, 5);
 
-  console.log(process.env.NODE_ENV);
   expect(result).toBe(9);
 });
